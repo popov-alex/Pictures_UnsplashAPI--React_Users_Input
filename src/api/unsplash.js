2acdc95673ccc6bad0f0bcb068aa85be6e29f144
Alexander Popov <ayp5xc@virginia.edu>
2022-08-01 11:22:35 -0400

    deleted unsplash.js with api key

--- src/api/unsplash.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "https://api.unsplash.com",
-  headers: {
-    Authorization: "Client-ID uxN4eVoAZ1fBMXNW_rnWk2FgZg-9uq40IgvmlNAYVuo",
-  },
-});
